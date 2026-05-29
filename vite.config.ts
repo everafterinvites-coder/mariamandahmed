c87f76b6ee687ee0096ecc7ab623fef78a867b01
everafterinvites <contact.everafterinvites@gmail.com>
2026-05-29 23:09:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/mariamandahmed/', // <-- ADDED THIS LINE RIGHT HERE
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
